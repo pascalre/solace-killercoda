edb5fdb5c577e50868d872ad62ed0584c707c866
Pascal Reitermann <pascalreitermann@gmail.com>
2026-07-23 11:32:16 +0200

    Add topic-taxonomy scenario

--- topic-taxonomy/setup.sh
+++ topic-taxonomy/setup.sh
@@ -1,10 +1,5 @@
 #!/bin/bash
-docker run -d -p 8080:8080 -p 55555:55555 -p '1943:1943' --shm-size=2g --name solace \
-  --ulimit core=-1 \
-  --ulimit nofile=2448:1048576 \
-  --env username_admin_globalaccesslevel=admin \
-  --env username_admin_password=admin \
-  solace/solace-pubsub-standard
+docker run -d -p 8080:8080 -p 55555:55555 --shm-size=1g --env username_admin_globalaccesslevel=admin --env username_admin_password=admin --name=solace --mount type=bind,source=/mnt/solace,destination=/var/lib/solace,ro=false --ulimit nofile=2448:1048576 solace/solace-pubsub-standard
 
 #Warten bis SEMP verfügbar ist
 until curl -s -o /dev/null http://localhost:8080/SEMP/v2/config/about; do sleep 2; done
